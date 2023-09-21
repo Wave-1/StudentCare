bea1842e46acaa59296129ac798b1f111ea94a2a
tan tran <you@example.com>
2023-09-21 16:03:25 +0700

    Update_Login

--- src/apiConfig.jsx
+++ src/apiConfig.jsx
@@ -2,7 +2,7 @@
 const API_BASE_URL = 'https://localhost:7281/api/';
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJRCI6IjEiLCJSb2xlSUQiOiIxIiwiVG9rZW5JRCI6IjRjZjQwM2QxLWJkODctNDY0MS1iZjU4LWMyMjI3NTA4MjQ0YSIsImV4cCI6MTY5NTEwMTU3NywiaXNzIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NzI4MSIsImF1ZCI6Imh0dHBzOi8vbG9jYWxob3N0OjcyODEifQ.UclNsxW2FjlVnSEWmbMk2nSBAR2PBikl4ZB_lB83910'
 const API_ROUTES = {
-    Login:'Auth/Login',
+    Login:'Authenticate/Login',
     WorkSchedule:'WorkSchedule',
     Teacher:'Teacher',
 }
